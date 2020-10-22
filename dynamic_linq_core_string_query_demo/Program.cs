25b34ff4124cf033c57d82093d2f32738fd441f8
暐翰 林 <shps951002@gmail.com>
2020-10-22 17:15:55 +0800

    add:Dynamic LINQ Core String Query Demo

---
+++ dynamic_linq_core_string_query_demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System;
+using System.Linq.Dynamic.Core;
+
+namespace dynamic_linq_core_string_query_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = Enumerable.Range(1,20).Select(s=>new{id=s,val=Guid.NewGuid()});
+            {
+                System.Console.WriteLine("where string query with parameter");
+                var r = d.AsQueryable().Where("id >= @0 && id <= @1",10,15);
+                foreach (var item in r)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }
+            {
+                System.Console.WriteLine("select string query");
+                var r = d.AsQueryable().Where("id >= @0 && id <= @1",10,15).Select("val");
+                foreach (var item in r)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }  
+            {
+                System.Console.WriteLine("select string query new object with function");
+                var r = d.AsQueryable().Where("id >= @0 && id <= @1",10,15).Select("new {val,val.ToString().Replace(\"-\",\"\") as newVal}");
+                foreach (var item in r)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }                          
+        }
+    }
+}
---
+++ dynamic_linq_core_string_query_demo/README.md
@@ -0,0 +1,17 @@
+# Dynamic LINQ Core String Query Demo
+Use `System.Linq.Dynamic.Core` to query/filter data without string.  
+
+## Youtube Demo Link:
+[https://youtu.be/qjXmclbQvGE](https://youtu.be/qjXmclbQvGE)
+
+## Advantage:
+- Lightweight  
+- String type means more flexible
+
+Suitable for dynamic query.
+
+## Notice:
+It only support `IQueryable`, so Enumerable type object need to cast by `AsQueryable` function.
+
+## Read Docs
+- [zzzprojects/System.Linq.Dynamic.Core: The .NET Standard / .NET Core version from the System Linq Dynamic functionality.](https://github.com/zzzprojects/System.Linq.Dynamic.Core)
